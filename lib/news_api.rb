c8f66c53b7cfc78ee43461c0402299881f3f41da
Emilia <e.andrzejewska@pilot.co>
2014-12-23 20:15:44 +0100

    #4 Implementing classic sinatra app in the most simple way

--- lib/news_api.rb
+++ lib/news_api.rb
@@ -1,5 +1,34 @@
+require 'sinatra'
+require 'json'
+
 class NewsApi
-  def call(env)
-    [200, {"Content-Type" => "text/html"}, ["Hello Rack!"]]
+  def initialize
+    @data = { stories:
+                [ { id: "1", title: "Lorem ipsum", url: "http://www.lipsum.com/" },
+                { id: "2", title: "Lorem", url: "http://www.lorem.com/" } ]
+            }
+  end
+
+  def all_stories
+    @data
+  end
+
+  def find_story(id)
+    @data[:stories].select { |story| story[:id] == id }.first
+  end
+end
+
+
+news_api = NewsApi.new
+
+get '/stories' do
+  news_api.all_stories.to_json
+end
+
+get '/stories/:id' do
+  if story = news_api.find_story(params['id'])
+    story.to_json
+  else
+    halt 404
   end
 end
