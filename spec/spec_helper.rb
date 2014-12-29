3d44ccd33f8557677dec433e1240457925af0379
Emilia <e.andrzejewska@pilot.co>
2014-12-29 14:11:04 +0100

    #6 Using database cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,15 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'news_api'
+require 'models/story'
 require 'rack/test'
+require 'database_cleaner'
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
